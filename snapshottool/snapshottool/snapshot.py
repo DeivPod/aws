4b838860f48d5e3e0879f55e0b464bf1a3192570
DeivPod <45049082+DeivPod@users.noreply.github.com>
2021-03-25 17:13:16 +0200

    try/except

--- snapshottool/snapshottool/snapshot.py
+++ snapshottool/snapshottool/snapshot.py
@@ -4,16 +4,19 @@ def create_instance_snapshot(tags):
     if not tags:
         return print("No tags were provided")
     else:
-        print ("The script will backup instances with tags:")
-        for i in tags:
-            print(f"Name: {i}, Value:{tags[i]}")
-        for i in tags:
-            for instance in ec2.instances.filter(
-                Filters=[{
-                    'Name': f"tag:{i}",
-                    'Values': [f"{tags[i]}"]
-                }]
-            ):
-                for device in instance.block_device_mappings:
-                    ec2.create_snapshot(VolumeId=device.get('Ebs').get('VolumeId'))
-            return print(f"Snapshots are being created, check EC2 console")
+        try:
+            print ("The script will backup instances with tags:")
+            for i in tags:
+                print(f"Name: {i}, Value:{tags[i]}")
+            for i in tags:
+                for instance in ec2.instances.filter(
+                    Filters=[{
+                        'Name': f"tag:{i}",
+                        'Values': [f"{tags[i]}"]
+                    }]
+                ):
+                    for device in instance.block_device_mappings:
+                        ec2.create_snapshot(VolumeId=device.get('Ebs').get('VolumeId'))
+                return print(f"Snapshots are being created, check EC2 console")
+        except:
+            print("Error occured")
