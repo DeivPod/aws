236c575d8a3ecd90aa1ab1f619a1d513417fda66
Deividas Poderis <deividas.poderis@centric.eu>
2021-03-25 16:25:16 +0200

    identation in setup.py

--- snapshottool/setup.py
+++ snapshottool/setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
- setup(
-   name='snapshottool',
-   version='0.1.0',
-   author='Deividas',
-   packages=['snapshottool'],
-   url='https://github.com/DeivPod/aws/snapshottool',
-   license='LICENSE.txt',
-   description='EC2 snapshot creator',
-   long_description=open('README.txt').read(),
-   install_requires=[
-       "Click",
-       "boto3",
-       "unzip"
-       "AWS CLI v2"
-   ],
+setup(
+  name='snapshottool',
+  version='0.1.0',
+  author='Deividas',
+  packages=['snapshottool'],
+  url='https://github.com/DeivPod/aws/snapshottool',
+  license='LICENSE.txt',
+  description='EC2 snapshot creator',
+  long_description=open('README.txt').read(),
+  install_requires=[
+    "Click",
+    "boto3",
+    "unzip"
+    "AWS CLI v2"
+  ],
 )
