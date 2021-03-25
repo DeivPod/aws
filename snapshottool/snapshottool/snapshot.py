9037981aab8a8f731332cf0bb50f346d6c75f702
DeivPod <45049082+DeivPod@users.noreply.github.com>
2021-03-25 17:09:54 +0200

    snapshot.py updated

--- snapshottool/snapshottool/snapshot.py
+++ snapshottool/snapshottool/snapshot.py
@@ -4,8 +4,10 @@ def create_instance_snapshot(tags):
     if not tags:
         return print("No tags were provided")
     else:
+        print ("The script will backup instances with tags:")
         for i in tags:
-            print(f"Name: {i}, Value:{tags[i]} ")
+            print(f"Name: {i}, Value:{tags[i]}")
+        for i in tags:
             for instance in ec2.instances.filter(
                 Filters=[{
                     'Name': f"tag:{i}",
@@ -14,6 +16,4 @@ def create_instance_snapshot(tags):
             ):
                 for device in instance.block_device_mappings:
                     ec2.create_snapshot(VolumeId=device.get('Ebs').get('VolumeId'))
-                return print(f"Snapshots are being created, check EC2 console")
-
-
+            return print(f"Snapshots are being created, check EC2 console")
